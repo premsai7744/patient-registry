3a7effc764a44fbb5de79a310bc0d7d9724753ed
premsai7744 <premsai7744@gmail.com>
2026-08-01 18:37:37 +0530

    Initial commit

---
+++ patient-registry/src/main/java/com/premit/patient_registry/PatientRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.premit.patient_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistryApplication.class, args);
+	}
+
+}
---
+++ patient-registry/src/test/java/com/premit/patient_registry/PatientRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.patient_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
